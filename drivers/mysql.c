c7d614264e8e588e40ebf4ccb7f92542864bb58a
mmt <mmt>
2001-03-29 02:07:22 +0000

    Updated mysql.c

--- drivers/mysql.c
+++ drivers/mysql.c
@@ -13,28 +13,80 @@ extern "C" {
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dbi.h"
 #include "mysqlclient.h"
 
-//typedef struct mysql_connection_s {
-//	MYSQL *connection;
-//	int handler;
-//	mysql_connection_t *next;
-//} mysql_connection_t;
+/*
+typedef struct mysql_connection_s { 
+ 	MYSQL *connection; 
+ 	int handler; 
+ 	struct mysql_connection_s *next; 
+} mysql_connection_t; 
 
-//mysql_connection_t *cons = NULL;
+ mysql_connection_t *cons = NULL; */
 
 
 int dbd_connect(dbi_driver_t *driver) /* host and login info already stored in driver's info table */
 {
-	MYSQL *con = mysql_init(NULL);
+	MYSQL *con=NULL; /* The mysql connection handler */
 
-	
+	char *host=NULL, *user=NULL, *password=NULL, *db=NULL; 
+	int port=0, flags = 0; /* This and the above variables will be used for the connection */
+
+	dbi_option_t *trav = driver->options; /* The option list */
+
+	*con = mysql_init(NULL);
+
+	if(con == NULL){
+		strcpy(driver->error_message, "Not Enough Memory To Allocate Handle");
+		driver->status = -1;
+		return -1;
+	}
+
+	/* This loops traverses the options and tries to set as many as possible for the connection */
+	while(trav){ 	
+		if(! strcmp(trav->key, "host") ){
+			strcpy(host, trav->string_value);
+			
+		} else if(! strcmp(trav->key, "username") ){
+			strcpy(user, trav->string_value);
+			
+		} else if(! strcmp(trav->key, "password") ){
+			strcpy(password, trav->string_value);
+
+		} else if(! strcmp(trav->key, "database") ){
+			strcpy(db, trav->string_value);
+		
+		} else if(! strcmp(trav->key, "port") ){
+			port = trav->numeric_value;
+			
+		} else if(! strcmp(trav->key, "use_compression") ){
+			if(trav->numeric_value) flags |= CLIENT_COMPRESS;
+			else{ flags |= CLIENT_COMPRESS; flags ^= CLIENT_COMPRESS; }
+		}
+
+		trav = trav->next;
+	}
+
+	if(mysql_real_connect(con, host, user, password, db, port, NULL, flags)){
+		driver->connection = (void*) con;
+		strcpy(driver->currentdb, db);
+		driver->status = 0;
+	} else {
+		strcpy(driver->error_message, mysql_error(con));
+		driver->error_number = mysql_errno(con);
+		driver->status = -1;
+	}
+
+	return driver->status;
 }
 
 int dbd_disconnect(dbi_driver_t *driver)
 {
-
+	if(driver->connecntion){
+		mysql_close((MYSQL*)driver->connection);
+	}
 }
 
 int dbd_fetch_field(dbi_result_t *result, const char *key, void *to)
