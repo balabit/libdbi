6d52c2d523a8f2e6f862537b7aad7e485ad26bf1
mmt <mmt>
2001-07-30 06:46:08 +0000

    Compiling, hiccups in main preventing testing

--- drivers/mysql/dbd_mysql.c
+++ drivers/mysql/dbd_mysql.c
@@ -17,8 +17,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
- * dbd_pgsql.c: PostgreSQL database support (using libpq)
- * Copyright (C) 2001, David Parker <david@neongoat.com>.
+ * dbd_mysql.c: MySQL database support (using libpq)
+ * Copyright (C) 2001, Mark Tobenkin <mark@brentwoodradio.com>
  * http://libdbi.sourceforge.net
  * 
  * $Id$
