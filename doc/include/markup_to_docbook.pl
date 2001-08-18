c4fc8feefeb1045061445f6441877608683d581c
dap24 <dap24>
2001-08-18 09:00:40 +0000

    Auto-documentation is now really auto. Started plugin guide.

--- doc/include/markup_to_docbook.pl
+++ doc/include/markup_to_docbook.pl
@@ -1,36 +1,71 @@
 #!/usr/bin/perl -w
 
-# XXX TODO skips any blocks with DESC-VERBATIM XXX
+use strict; 
 
 local *DOCBOOK;
+local *MARKUP;
 
-parse_markup($ARGV[0]);
+inject_markup($ARGV[0], $ARGV[1]);
+
+sub inject_markup {
+	my $template = shift;
+	my $markup = shift;
+
+	my $docbookout = $template;
+	$docbookout =~ s/\.tmpl$/.sgml/;
+
+	open(MARKUP, "< $markup");
+	open(TMPL, "< $template");
+	open(DOCBOOK, "> $docbookout");
+	
+	while (<TMPL>) {
+		my $line = $_;
+		if ($line =~ /^(\s*)%%(\w+)\s*$/) {
+			my $func = $2;
+			my $tabs = $1;
+			my $numtabs = 0;
+			if (defined($tabs) && $tabs) {
+				$numtabs++ while ($tabs =~ /\t/g);
+			}
+			print DOCBOOK parse_markup($numtabs, $func) . "\n";
+		}
+		else {
+			print DOCBOOK $line;
+		}
+	}
+	
+	close(DOCBOOK);
+	close(TMPL);
+	close(MARKUP);
+	print "Done: $docbookout\n";
+}
 
 sub parse_markup {
-	my $filename = shift;
-	open(MARKUP, "< $filename");
-	open(DOCBOOK, "> reference.sgml-$$");
+	my $extratab = shift;
+	my $function = shift;
 	local $/ = "*\n";
+	seek(MARKUP, 0, 0);
 	while (<MARKUP>) {
 		my ($proto, $descverbatim, $desc, $returns, @args);
 		my $blob = $_; $blob =~ s/\n\*\n/\n/g;
 		($proto, $desc, $returns) = (($blob =~ /\tPROTO (.+)\n\tDESC[-\w]*\n\t(.*)ENDDESC\n\t.*RET (.*)[\s\*]*/s) && (strip($1), strip($2), strip($3)));
+		my $funcbasename = (($proto =~ /\w+ \**(\w+)\(.*\)/) && $1);
+		next unless $funcbasename eq $function;
 		$descverbatim = (($blob =~ /\n\t(DESC[-\w]*)\n/) && ($1 eq 'DESC-VERBATIM'));
-		my $argsraw = (($blob =~ /\n\tENDDESC\n\t(ARG .*)\n\tRET /) && $1);
+		my $argsraw = (($blob =~ /\n\tENDDESC\n\t(ARG .*)\n\tRET /s) && $1);
 		foreach my $arg (split(/\n/, $argsraw)) {
 			$arg = strip($arg);
 			$arg =~ s/^\s*ARG (\w+ .+)\s*$/$1/;
 			push(@args, $arg);
 		}
-		spit_out_docbook($proto, $descverbatim, $desc, $returns, @args) unless strip($proto) eq "";
+		#print "SENDING ARGS FOR $funcbasename: " . join(" -- ", @args) . "\n";
+		return spit_out_docbook($extratab, $proto, $descverbatim, $desc, $returns, @args);
 	}
-	print DOCBOOK "\n";
-	close(DOCBOOK);
-	close(MARKUP);
-	print "Done: reference.sgml-$$\n";
+	return "";
 }
 
 sub spit_out_docbook {
+	my $extratab = shift;
 	my $funcproto = shift;
 	my $descraw = shift;
 	my $desc = shift;
@@ -39,42 +74,47 @@ sub spit_out_docbook {
 
 	my $funcbasename = (($funcproto =~ /\w+ \**(\w+)\(.*\)/) && $1);
 	my $functoken = $funcbasename; $functoken =~ s/_/-/g;
-	my $tabs = "\t"x2; #x3;
+	my $tabs = "\t"x$extratab; #x2;
 
-	print DOCBOOK "\n$tabs<Section id=\"$functoken\" XRefLabel=\"$funcbasename\"><Title>$funcbasename</Title>\n$tabs\t<Para><ProgramListing>$funcproto</ProgramListing></Para>";
+	my $OUTPUT;
+
+	$OUTPUT .= "$tabs<Section id=\"$functoken\" XRefLabel=\"$funcbasename\"><Title>$funcbasename</Title>\n$tabs\t<Para><ProgramListing>$funcproto</ProgramListing></Para>";
 	if (defined($desc) && ($desc ne "")) {
 		if ($descraw) {
-			print DOCBOOK "\n$tabs\t$desc";
+			foreach my $line (split(/\n/, $desc)) {
+				$OUTPUT .= "\n$tabs\t$line";
+			}
 		}
 		else {
-			print DOCBOOK "\n$tabs\t<Para>$desc</Para>";
+			$OUTPUT .= "\n$tabs\t<Para>$desc</Para>";
 		}
 	}
 	if ((defined(@arguments) && (scalar(@arguments) > 0)) || (defined($returns) && ($returns ne ""))) {
-		print DOCBOOK "\n$tabs\t<VariableList>";
+		$OUTPUT .= "\n$tabs\t<VariableList>";
 		if (scalar(@arguments) > 0) {
-			print DOCBOOK "\n$tabs\t\t<VarListEntry>";
-			print DOCBOOK "\n$tabs\t\t\t<Term><Emphasis>Arguments</Emphasis></Term>";
-			print DOCBOOK "\n$tabs\t\t\t<ListItem>";
+			$OUTPUT .= "\n$tabs\t\t<VarListEntry>";
+			$OUTPUT .= "\n$tabs\t\t\t<Term><Emphasis>Arguments</Emphasis></Term>";
+			$OUTPUT .= "\n$tabs\t\t\t<ListItem>";
 			foreach (@arguments) {
 				my $line = strip($_);
 				my ($token, $more) = (($line =~ /^(\w+)\s+(.+)$/) && (strip($1), strip($2)));
 				if ($token ne "") {
-					print DOCBOOK "\n$tabs\t\t\t\t<Para><Literal>$token</Literal>: $more</Para>";
+					$OUTPUT .= "\n$tabs\t\t\t\t<Para><Literal>$token</Literal>: $more</Para>";
 				}
 			}
-			print DOCBOOK "\n$tabs\t\t\t</ListItem>";
-			print DOCBOOK "\n$tabs\t\t</VarListEntry>";
+			$OUTPUT .= "\n$tabs\t\t\t</ListItem>";
+			$OUTPUT .= "\n$tabs\t\t</VarListEntry>";
 		}
 		if ($returns ne "") {
-			print DOCBOOK "\n$tabs\t\t<VarListEntry>";
-			print DOCBOOK "\n$tabs\t\t\t<Term><Emphasis>Returns</Emphasis></Term>";
-			print DOCBOOK "\n$tabs\t\t\t<ListItem><Para>$returns</Para></ListItem>";
-			print DOCBOOK "\n$tabs\t\t</VarListEntry>";
+			$OUTPUT .= "\n$tabs\t\t<VarListEntry>";
+			$OUTPUT .= "\n$tabs\t\t\t<Term><Emphasis>Returns</Emphasis></Term>";
+			$OUTPUT .= "\n$tabs\t\t\t<ListItem><Para>$returns</Para></ListItem>";
+			$OUTPUT .= "\n$tabs\t\t</VarListEntry>";
 		}
-		print DOCBOOK "\n$tabs\t</VariableList>";
+		$OUTPUT .= "\n$tabs\t</VariableList>";
 	}
-	print DOCBOOK "\n$tabs</Section>";
+	$OUTPUT .= "\n$tabs</Section>";
+	return $OUTPUT;
 }
 
 sub strip {
