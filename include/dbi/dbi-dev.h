c55e697192dfaaa7058b5c35d58978c04ee3cdde
mhoenicka <mhoenicka>
2005-05-22 20:02:12 +0000

    added pointers to dbd_encoding_from_iana() and dbd_encoding_to_iana()

--- include/dbi/dbi-dev.h
+++ include/dbi/dbi-dev.h
@@ -122,6 +122,8 @@ typedef struct dbi_functions_s {
 	dbi_result_t *(*query_null)(dbi_conn_t_pointer, const unsigned char *, unsigned long);
 	int (*quote_string)(dbi_driver_t_pointer, const char *, char *);
 	int (*conn_quote_string)(dbi_conn_t_pointer, const char *, char *);
+        const char* (*encoding_to_iana)(const char *);
+        const char* (*encoding_from_iana)(const char *);
 	char *(*select_db)(dbi_conn_t_pointer, const char *);
 	int (*geterror)(dbi_conn_t_pointer, int *, char **);
 	unsigned long long (*get_seq_last)(dbi_conn_t_pointer, const char *);
